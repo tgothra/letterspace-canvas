ac75fc3756beda4c01803dd763230d20a953bd4d
Timothy <tgothra@icloud.com>
2025-08-09 12:11:26 -0400

    fix(mac): remove duplicate PlatformImage typealias; rely on shared definition

--- Letterspace Canvas/Views/Components/CircularMenuButton.swift
+++ Letterspace Canvas/Views/Components/CircularMenuButton.swift
@@ -2,10 +2,8 @@ import SwiftUI
 
 #if os(iOS)
 import UIKit
-typealias PlatformImage = UIImage
 #elseif os(macOS)
 import AppKit
-typealias PlatformImage = NSImage
 #endif
 
 struct CircularMenuButton: View {
